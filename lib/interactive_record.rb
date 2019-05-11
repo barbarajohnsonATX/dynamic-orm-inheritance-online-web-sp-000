d02a9e8e27f41cb3e77e330ce569faa718fce939
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-11 03:57:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,8 +21,7 @@ class InteractiveRecord
   end
 
   def initialize(options={})
-    binding.pry
-    options.each do |property, value|
+     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
